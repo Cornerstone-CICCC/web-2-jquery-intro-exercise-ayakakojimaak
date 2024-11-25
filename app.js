586d091befd2bbbb5221218e68d92139f15292ee
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-11-25 11:51:34 -0800

    add toggleButton

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-$(function() {
+$(function () {
+  $("#toggleButton").on("click", () => {
+    if ($("#myParagraph").css("display") === "block") {
+      $("#myParagraph").hide();
+    } else {
+      $("#myParagraph").show();
+    }
+  });
+
+  $("#toggleButton").on("colorButton", () => {});
   // YOUR CODE HERE
-})
+});
